033553e9290f953ed7bc3e743d8a0e288807ee12
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-30 18:40:45 +0530

    get user github profile --function -----ADDED

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -312,4 +312,30 @@ router.delete('/', auth, async (req, res) => {
 });
 
 
+/**
+*  @route        GET api/profile/github/:username
+*  @desc         Get user repos from GitHub
+*  @access       Public
+**/
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) return res.status(404).json({ message: 'No GitHub profile found' });
+
+            res.json(JSON.parse(body));
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
