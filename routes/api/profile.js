dfe52e8f70874c5f136b17b48ee178b2b47c428f
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-30 18:14:51 +0530

    add & delete education --function -----ADDED

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -216,6 +216,70 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 
 
 /**
+*  @route        PUT api/profile/education
+*  @desc         Add profile education
+*  @access       Private
+**/
+router.put('/education',
+    // middleware for authentication and validation
+    [auth,
+        [
+            check('school', 'School is required').not().isEmpty(),
+            check('degree', 'Degree is required').not().isEmpty(),
+            check('from', 'From Date is required').not().isEmpty(),
+            check('fieldofstudy', 'From Date is required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+        const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+
+        const newEdu = { school, degree, fieldofstudy, from, to, current, description };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            profile.education.unshift(newEdu);
+
+            await profile.save();
+
+            res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            res.status(200).send('Server Error');
+        }
+    });
+
+
+/**
+*  @route        DELETE api/profile/education/:edu_id
+*  @desc         Delete education from profile
+*  @access       Private
+**/
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get the remove index
+        // for every entry in the education mapped to an array based on the ObjectId, and getting the specific 
+        // object's index in the education array using the edu_id
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+        // based on the indexOf the education object, splice it to remove it
+        profile.education.splice(removeIndex, 1);
+        // saving the modified document
+        await profile.save();
+        // return response
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+/**
 *  @route        DELETE api/profile
 *  @desc         Delete profile, user and posts
 *  @access       Private
