fb792d96598518e478cbca90eeb528010167f11d
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-30 14:56:41 +0530

    delete profile & user function added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,4 +152,26 @@ router.get('/user/:user_id', async (req, res) => {
 });
 
 
+/**
+*  @route        DELETE api/profile
+*  @desc         Delete profile, user and posts
+*  @access       Private
+**/
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+
+        // Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        // Remove User
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ message: "User deleted" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 module.exports = router;
