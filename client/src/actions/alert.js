0c8526772c59afd9acce0ab6ce4d42231e7bfaaf
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-07-31 17:01:37 +0530

    timeout props --state/reducer -----implemented

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from "./types";
 
 import { v4 as uuidv4 } from 'uuid';
 
-export const setAlert = (message, alertType) => dispatch => {
+export const setAlert = (message, alertType, timeout = 3000) => dispatch => {
     const id = uuidv4();
     // set an action, that takes message, alertType, and id that is generated
     dispatch({
@@ -11,5 +11,5 @@ export const setAlert = (message, alertType) => dispatch => {
     });
     // on every setAlert being fired for SET_ALERT, dispatch an action to
     // remove the alert after 3 seconds
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 3000);
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
