f04b485a5519d0563fbf174b19f63b0ab1fd6644
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-01 23:37:29 +0530

    comments

---
+++ client/src/components/profile-forms/AddEducation.js
@@ -0,0 +1,140 @@
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+// to declare type of property
+import PropTypes from 'prop-types';
+// to establish connection with redux, also export
+import { connect } from 'react-redux';
+// addEducation action to add education to the profile
+import { addEducation } from '../../actions/profile';
+
+const AddEducation = ({ addEducation, history }) => {
+  // initial state for formData
+  const [formData, setFormData] = useState({
+    school: '',
+    degree: '',
+    fieldofstudy: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+  //   state to toggle current job button
+  const [toDateDisabled, toggleDisabled] = useState(false);
+  //   destructure from formData
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  } = formData;
+  //   onChange event handler for inputs
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    //   inline submit event done
+    <Fragment>
+      <h1 class='large text-primary'>Add Your Education</h1>
+      <p class='lead'>
+        <i class='fas fa-code-branch'></i> Add any school or bootcamp that you
+        have attended
+      </p>
+      <small>* = required field</small>
+      <form
+        class='form'
+        onSubmit={(e) => {
+          e.preventDefault();
+          addEducation(formData, history);
+        }}
+      >
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='* Degree or Certificate'
+            name='degree'
+            value={degree}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='* School'
+            name='school'
+            value={school}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div class='form-group'>
+          <input
+            type='text'
+            placeholder='Field of Study'
+            name='fieldofstudy'
+            value={fieldofstudy}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <h4>From Date</h4>
+          <input
+            type='date'
+            name='from'
+            value={from}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div class='form-group'>
+          <p>
+            <input
+              type='checkbox'
+              name='current'
+              value=''
+              value={current}
+              onChange={(e) => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+              checked={current}
+            />{' '}
+            Current Job
+          </p>
+        </div>
+        <div class='form-group'>
+          <h4>To Date</h4>
+          <input
+            type='date'
+            name='to'
+            value={to}
+            onChange={(e) => onChange(e)}
+            disabled={toDateDisabled ? 'disabled' : ''}
+          />
+        </div>
+        <div class='form-group'>
+          <textarea
+            name='description'
+            cols='30'
+            rows='5'
+            placeholder='Program Description'
+            value={description}
+            onChange={(e) => onChange(e)}
+          ></textarea>
+        </div>
+        <input type='submit' class='btn btn-primary my-1' />
+        <a class='btn btn-light my-1' href='dashboard.html'>
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
+};
+
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addEducation })(AddEducation);
