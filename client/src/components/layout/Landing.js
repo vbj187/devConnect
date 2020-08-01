0923a8ea41ef90e0fd78046efe2c8178debef528
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-01 17:06:24 +0530

    when auth, restricts from dashboard

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,25 +1,46 @@
 import React from 'react';
 // import Link to use instead of "a" tags
-import { Link } from "react-router-dom";
+import { Link, Redirect } from 'react-router-dom';
+// to establish connection with redux, also export
+import { connect } from 'react-redux';
+// to declare type of property
+import PropTypes from 'prop-types';
 
-const Landing = () => {
-    return (
-        <section className="landing">
-            <div className="dark-overlay">
-                <div className="landing-inner">
-                    <h1 className="x-large">DevConnect</h1>
-                    <p className="lead">
-                        Create a developer profile/portfolio, share posts and get help from
-                        other developers
-                    </p>
-                    <div className="buttons">
-                        <Link to="/register" className="btn btn-primary">Sign Up</Link>
-                        <Link to="/login" className="btn btn-light">Login</Link>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+const Landing = ({ isAuthenticated }) => {
+  // to restrict user from opening landing page when authenticated
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>DevConnect</h1>
+          <p className='lead'>
+            Create a developer profile/portfolio, share posts and get help from
+            other developers
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,53 +1,68 @@
 import React, { Fragment } from 'react';
 // import Link to use instead of "a" tags
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 // to establish connection with redux, also export
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 // to declare type of property
 import PropTypes from 'prop-types';
 // import logout action
-import { logout } from "../../actions/auth";
+import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-    // conditionally render links based on authorization
-    const authLinks = (
-        <ul>
-            <li>
-                <a onClick={logout} href="#!">
-                    <i className='fas fa-sign-out-alt'></i>{' '}
-                    <span className='hide-sm'>Logout</span>
-                </a>
-            </li>
-        </ul>
-    );
-    // conditionally render links based on authorization
-    const guestLinks = (
-        <ul>
-            <li><a href="#!">Developers</a></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Login</Link></li>
-        </ul>
-    );
+  // conditionally render links based on authorization
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+  // conditionally render links based on authorization
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='#!'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
 
-    return (
-        <nav className="navbar bg-dark">
-            <h1>
-                <Link to="/"><i className="fas fa-code"></i> DevConnect</Link>
-            </h1>
-            {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
-        </nav>
-    );
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code'></i> DevConnect
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
 };
 
 // declaring propTypes
 Navbar.propTypes = {
-    logout: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
-
-const mapStateToProps = state => ({
-    auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
