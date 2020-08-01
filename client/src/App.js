0f0db2936da8bdd637e421bb7ebb365b7130f6e5
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-01 18:17:50 +0530

    create profile --component -----added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 // Provider connects redux to react
 import { Provider } from 'react-redux';
 // to use the managed states
@@ -45,6 +46,11 @@ const App = () => {
               <Route exact path='/login' component={Login} />
               {/* for PROTECTED ROUTING */}
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
