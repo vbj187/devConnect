8b5c9f222d803ebfe44787273c6d8e363f9ceef2
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-01 18:24:43 +0530

    toggle social links --conditionalRendering -----ADDED

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -18,6 +18,8 @@ const CreateProfile = (props) => {
     instagram: '',
   });
 
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
   const {
     company,
     website,
@@ -99,36 +101,46 @@ const CreateProfile = (props) => {
         </div>
 
         <div className='my-2'>
-          <button type='button' className='btn btn-light'>
+          {/* toggle view on change */}
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
+        {/* conditional rendering for toggle social links */}
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input type='text' placeholder='Twitter URL' name='twitter' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-twitter fa-2x'></i>
-          <input type='text' placeholder='Twitter URL' name='twitter' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input type='text' placeholder='Facebook URL' name='facebook' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-facebook fa-2x'></i>
-          <input type='text' placeholder='Facebook URL' name='facebook' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x'></i>
+              <input type='text' placeholder='YouTube URL' name='youtube' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-youtube fa-2x'></i>
-          <input type='text' placeholder='YouTube URL' name='youtube' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input type='text' placeholder='Linkedin URL' name='linkedin' />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-linkedin fa-2x'></i>
-          <input type='text' placeholder='Linkedin URL' name='linkedin' />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input type='text' placeholder='Instagram URL' name='instagram' />
+            </div>
+          </Fragment>
+        )}
 
-        <div className='form-group social-input'>
-          <i className='fab fa-instagram fa-2x'></i>
-          <input type='text' placeholder='Instagram URL' name='instagram' />
-        </div>
         <input type='submit' className='btn btn-primary my-1' />
         <a className='btn btn-light my-1' href='dashboard.html'>
           Go Back
