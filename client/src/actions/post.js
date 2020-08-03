6fa46d9e6a475b4ca4e6e9fe5bfbb2b5161d9d14
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-03 23:14:01 +0530

    like & unlike --action--reducers--component -----added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import setAlert from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -17,6 +17,46 @@ export const getPosts = () => async (dispatch) => {
       payload: {
         msg: err.response.statusText,
         status: err.response.status,
+      },
+    });
+  }
+};
+
+// Like a post
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, like: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// UnLike a post
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, like: res.data },
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
       },
     });
   }
