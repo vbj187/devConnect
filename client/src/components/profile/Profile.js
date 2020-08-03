bf46e161ef971b06698bf9cd769ba3ab141c6836
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-03 18:05:52 +0530

    profile experience --component -----added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -36,6 +38,21 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience.id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>Nothing to show</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
