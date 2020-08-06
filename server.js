b05b6585709f1da1a74d2c6758fa5825ddd81e2d
Vigneshwaran Balamurugan <vbj1807@gmail.com>
2020-08-06 22:44:21 +0530

    commment

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ connectDB();
 
 // Init middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('API running'));
-
+// Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
